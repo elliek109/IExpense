315fce796626477229d90dfd6f7e23f954cf710b
ellie <elaina.kuhnke@stu.barren.kyschools.us>
2025-11-03 09:32:04 -0600

    Initial Commit

---
+++ IExpense/IExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  IExpenseApp.swift
+//  IExpense
+//
+//  Created by Student on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct IExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IExpenseTests/IExpenseTests.swift
@@ -0,0 +1,17 @@
+//
+//  IExpenseTests.swift
+//  IExpenseTests
+//
+//  Created by Student on 11/3/25.
+//
+
+import Testing
+@testable import IExpense
+
+struct IExpenseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
